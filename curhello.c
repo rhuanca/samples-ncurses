5c67fdf923829d8374c2f3c9cc1be4ac16551fd9
Renan Huanca <rhuanca@gmail.com>
2014-11-22 20:14:15 -0500

    now we created a struct to support the clocks

--- curhello.c
+++ curhello.c
@@ -1,4 +1,15 @@
 #include <curses.h>
+#include <string.h>
+
+typedef struct clock_struct {
+	char title[50];
+	WINDOW *bol_time_win;
+} clock;
+
+
+void init_clock(WINDOW *win, char *title, struct clock_struct *clock) {
+	strcpy(clock->title, title);
+}
 
 WINDOW *init_newwin(int height, int width, int starty, int startx) {
 	WINDOW *local_win;
@@ -10,7 +21,9 @@ WINDOW *init_newwin(int height, int width, int starty, int startx) {
 	return local_win;
 }
 
+
 int main() {
+	clock bol_time, usa_est_time;
 	WINDOW *bol_time_win, *usa_est_time_win;
 	int maxrows, maxcols;
 	initscr();
@@ -21,7 +34,12 @@ int main() {
 	bol_time_win = init_newwin(maxrows, maxcols / 2, 0, 0);
 	usa_est_time_win = init_newwin(maxrows, maxcols / 2 - 1, 0, maxcols / 2 + 1);
 
+	init_clock(bol_time_win, "Bolivia Time", &bol_time);
 
+	mvwprintw(bol_time_win, 1, 1, "%s", bol_time.title );
+	//mvwprintw(bol_time_win, 1, 1, " hello again...");
+	refresh();
+	wrefresh(bol_time_win);
 	// printw("Hello World !!! %d %d", maxrows, maxcols); /* Print Hello World              */
 	refresh(); /* Print it on to the real screen */
 	getch(); /* Wait for user input */
